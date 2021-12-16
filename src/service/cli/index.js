d082a56aba78068f69eb7a0f281e13220f6ea290
lysenkod <denys.lysenko@contractors.roche.com>
2021-12-16 21:05:22 +0200

    2.16. Личный проект: сервер наносит первый удар

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const generate = require(`./generate`);
+const server = require(`./server`);
 const version = require(`./version`);
 const help = require(`./help`);
 
 module.exports.Cli = {
   [generate.name]: generate,
+  [server.name]: server,
   [version.name]: version,
   [help.name]: help,
 };
