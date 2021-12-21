8d55c75101bd0d516fe275b81135e0a6ca5066ec
lysenkod <denys.lysenko@contractors.roche.com>
2021-12-21 19:15:24 +0200

    code review updates

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -2,21 +2,13 @@
 
 const {Router} = require(`express`);
 const mainRoute = require(`./main`);
-const registerRoute = require(`./register`);
-const loginRoute = require(`./login`);
 const myRoute = require(`./my`);
 const articlesRoute = require(`./articles`);
-const searchRoute = require(`./search`);
-const categoriesRoute = require(`./categories`);
 
 const router = new Router();
 
 router.use(`/`, mainRoute);
-router.use(`/register`, registerRoute);
-router.use(`/login`, loginRoute);
 router.use(`/my`, myRoute);
 router.use(`/articles`, articlesRoute);
-router.use(`/search`, searchRoute);
-router.use(`/categories`, categoriesRoute);
 
 module.exports = router;
--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -8,4 +8,20 @@ mainRoute.get(`/`, (req, res) => {
   res.send(req.originalUrl);
 });
 
+mainRoute.get(`/register`, (req, res) => {
+  res.send(req.originalUrl);
+});
+
+mainRoute.get(`/login`, (req, res) => {
+  res.send(req.originalUrl);
+});
+
+mainRoute.get(`/search`, (req, res) => {
+  res.send(req.originalUrl);
+});
+
+mainRoute.get(`/categories`, (req, res) => {
+  res.send(req.originalUrl);
+});
+
 module.exports = mainRoute;
